28221c0acbd209d1ebd17666460c0d53854bada7
IJAHARKHAN <31922699+IJAHARKHAN@users.noreply.github.com>
2023-06-10 15:37:41 +0000

    createFile varriables.js

---
+++ 01_basics/varriables.js
@@ -0,0 +1,8 @@
+const accountId = 123456;
+let accountEmail = 'khan7oct@gmail.com';
+var accountPassword = '321465';
+accountCity = 'Deoria';
+// accountId = 32146;
+let accountState;
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
