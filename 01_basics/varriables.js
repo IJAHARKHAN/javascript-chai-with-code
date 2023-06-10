7e7d3ad32206b623d3ee73fe5714164e0b82ec1f
IJAHARKHAN <31922699+IJAHARKHAN@users.noreply.github.com>
2023-06-10 15:45:02 +0000

    let put comments in varriables.js

--- 01_basics/varriables.js
+++ 01_basics/varriables.js
@@ -5,4 +5,14 @@ accountCity = 'Deoria';
 // accountId = 32146;
 let accountState;
 
+ accountEmail = 'khan7oct@outlook.com';
+ accountPassword = '321654';
+ accountCity = 'Lucknow'
+
+
+ /*
+prefer not to use var
+beacase of issue in block scope and function scope
+ */
+
 console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
